c0ff9a534a48d1246fb87216826aa997ea353891
Антон <@ANTONPK>
2025-08-22 10:17:46 +0500

    Обновлены комментарии

--- RoomFilter.cs
+++ RoomFilter.cs
@@ -10,16 +10,34 @@ using Autodesk.Revit.UI.Selection;
 
 namespace RevitTest2
 {
+    /// <summary>
+    /// Фильтр выбора элементов, позволяющий выбирать только помещения (Room)
+    /// </summary>
     public class RoomFilter : ISelectionFilter
     {
+        #region Методы ISelectionFilter
+
+        /// <summary>
+        /// Определяет, можно ли выбрать элемент
+        /// </summary>
+        /// <param name="elem">Проверяемый элемент</param>
+        /// <returns>True, если элемент является помещением (Room), иначе False</returns>
         public bool AllowElement(Element elem)
         {
             return elem is Room;
         }
 
+        /// <summary>
+        /// Определяет, можно ли выбрать ссылку на элемент
+        /// </summary>
+        /// <param name="reference">Ссылка на элемент</param>
+        /// <param name="position">Позиция выбора</param>
+        /// <returns>Всегда возвращает False, так как разрешен только выбор элементов, а не ссылок</returns>
         public bool AllowReference(Reference reference, XYZ position)
         {
             return false;
         }
+
+        #endregion
     }
 }
--- RoomItem.cs
+++ RoomItem.cs
@@ -8,21 +8,47 @@ using System.Threading.Tasks;
 
 namespace RevitTest2
 {
+    /// <summary>
+    /// Класс-обертка для помещения (Room) с поддержкой выбора
+    /// </summary>
     public class RoomItem : BaseViewModel
     {
+        #region Поля
+
         private bool _isSelected;
 
+        #endregion
+
+        #region Свойства
+
+        /// <summary>
+        /// Помещение Revit
+        /// </summary>
         public Room Room { get; }
+
+        /// <summary>
+        /// Флаг, указывающий выбрано ли помещение
+        /// </summary>
         public bool IsSelected
         {
             get => _isSelected;
             set => SetField(ref _isSelected, value);
         }
 
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Инициализирует новый экземпляр RoomItem
+        /// </summary>
+        /// <param name="room">Помещение Revit</param>
         public RoomItem(Room room)
         {
             Room = room;
             IsSelected = false;
         }
+
+        #endregion
     }
 }
